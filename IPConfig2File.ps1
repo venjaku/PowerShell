d35abd310d5984f22e1a321cb0822241f6b32b55
Venkatesh Vanjaku <vvronline.in@gmail.com>
2020-03-06 22:58:52 +0530

    Update IPConfig2File.ps1

--- IPConfig2File.ps1
+++ IPConfig2File.ps1
@@ -1,3 +1,8 @@
 ﻿<# Executing ipconfig and saving the results in the current path #>
 
 ipconfig > IPConfig.txt
+
+Write-Host "IPConfig.txt file created succesfully"
+
+"`n"
+Read-Host -Prompt "Press Enter to exit"
